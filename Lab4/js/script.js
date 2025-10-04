d94720799d9e20dfe60e44e3267fc3ce97a3077c
Zach <zjpk23@gmail.com>
2025-10-04 16:53:15 -0700

    Lab4

--- Lab4/js/script.js
+++ Lab4/js/script.js
@@ -1,14 +1,35 @@
 let zipInput = document.querySelector("#zip");
 zipInput.addEventListener("input", getZipInfo);
+
 let passwordInput = document.querySelector("#password");
-passwordInput.addEventListener("mouseover", showSuggestedPW);
-passwordInput.addEventListener("mouseout", hideSuggestedPW);
+passwordInput.addEventListener("focus", showSuggestedPW);
+passwordInput.addEventListener("blur", hideSuggestedPW);
+// passwordInput.addEventListener("input", checkPassword);
+
+let passwordError = document.querySelector("#password-error");
+// passwordInput.addEventListener("focus", checkPassword);
+// passwordInput.addEventListener("blur", hidePasswordError);
+
+let retypeInput = document.querySelector("#retype-password");
+// retypeInput.addEventListener("input", checkRetypedPassword);
+
 let usernameInput = document.querySelector("#username");
-usernameInput.addEventListener("input", checkUsername);
+// usernameInput.addEventListener("input", checkUsername);
+
 let stateInput = document.querySelector("#states-select")
 stateInput.addEventListener("input", makeCountyList);
 
+let zipError = document.querySelector("#zip-error");
+zipError.style.display = "none";
+
+let submitBtn = document.querySelector("#sign-up");
+submitBtn.addEventListener("click", checkPassword);
+submitBtn.addEventListener("click", checkRetypedPassword);
+submitBtn.addEventListener("click", checkUsername);
+
+
 async function setupForm() {
+
     try {
         let statesResponse = await fetch("https://csumb.space/api/allStatesAPI.php");
 
@@ -28,16 +49,19 @@ async function setupForm() {
             stateOption.textContent = stateData.state;
             statesSelect.appendChild(stateOption);
         }
-
+        makeCountyList();
+        hideSuggestedPW();
+        hidePasswordError();
 
     } catch (apiError) {
         console.error(apiError);
     }
 }
+setupForm();
 
 async function makeCountyList() {
     try {
-        let queryString = "https://csumb.space/api/countyListAPI.php?state=" + stateInput.value; 
+        let queryString = "https://csumb.space/api/countyListAPI.php?state=" + stateInput.value;
         let countiesResponse = await fetch(queryString);
 
         if (!countiesResponse.ok) {
@@ -64,7 +88,7 @@ async function makeCountyList() {
     }
 }
 
-setupForm();
+
 
 async function getZipInfo() {
 
@@ -78,6 +102,12 @@ async function getZipInfo() {
 
         let zipData = await zipResponse.json();
         console.log(zipData);
+
+        if (!zipData) {
+            zipError.style.display = "inline";
+        } else {
+            zipError.style.display = "none";
+        }
 
         document.querySelector("#city").textContent = zipData.city;
         document.querySelector("#latitude").textContent = zipData.latitude;
@@ -96,35 +126,85 @@ async function showSuggestedPW() {
 
         let passwordData = await passwordResponse.json();
 
-        document.querySelector("#suggest-pw").textContent = passwordData.Password;
+        let suggestedPW = document.querySelector("#suggested-pw");
+        suggestedPW.textContent = `Suggested Password: ${passwordData.password}`;
+        suggestedPW.style.color = "grey";
+        suggestedPW.style.display = "flex";
+
     } catch (apiError) {
         console.error(apiError);
     }
 }
 function hideSuggestedPW() {
-    document.querySelector("#suggest-pw").textContent = "";
+    let suggestedPassword = document.querySelector("#suggested-pw")
+    suggestedPassword.style.display = "none";
+
+}
+function checkPassword() {
+    passwordError.style.display = "flex";
+    if (passwordInput.value.length == 0) {
+        passwordError.textContent = "";
+        return;
+    }
+    if (passwordInput.value.length < 6) {
+        passwordError.textContent = "Password too short!";
+        passwordError.style.color = "orange";
+        return;
+    } else {
+        passwordError.textContent = "";
+    }
+    
+}
+function hidePasswordError(){
+    passwordError.style.display = "none";
+}
+function checkRetypedPassword() {
+    let input = retypeInput;
+    let retypeError = document.querySelector("#retype-error");
+    if (input.value == "") {
+        retypeError.textContent = "";
+        return;
+    }
+    if(input.value === passwordInput.value){
+        retypeError.textContent = "Passwords match!";
+    }else{
+        retypeError.textContent = "Error: Inputs don't match!"
+    }
+
+
 }
 
 async function checkUsername() {
+    let outputMessage = document.querySelector("#error-message")
+    if (usernameInput.value.length == 0) {
+        outputMessage.textContent = "";
+        return;
+    }
+    if (usernameInput.value.length < 3) {
+        outputMessage.textContent = "Username too short!";
+        outputMessage.style.color = "orange";
+        return;
+    }
+
     try {
         let queryString = "https://csumb.space/api/usernamesAPI.php?username=" + usernameInput.value;
         let usernameResponse = await fetch(queryString);
 
-        if (!usernameResponse.ok){
+        if (!usernameResponse.ok) {
             throw new Error("Response Failed");
         }
 
         let usernameData = await usernameResponse.json();
 
-        let outputMessage = document.querySelector("#error-message")
-        if(!usernameData.available){
+
+        if (!usernameData.available) {
             outputMessage.textContent = "Username unavailable!";
             outputMessage.style.color = "red";
-        }else{
+        } else {
             outputMessage.textContent = "Username available!";
-            outputMessage.style.color = "grey";
+            outputMessage.style.color = "green";
         }
-        
+
 
     } catch (apiError) {
         console.error(apiError);
